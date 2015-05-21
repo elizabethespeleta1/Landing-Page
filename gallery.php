9c81f0eef583ba3084ee80edec4951a3cc318d3e
elizabeth espeleta <elizabeth.espeleta@gmail.com>
2015-05-21 10:53:20 -0700

    finished for the basic outline for home page

---
+++ gallery.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kawaii</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width=device-width"/>
+</head>
+<body>
+	<nav>
+		<ul class="ul">
+			<li class="li"><a href="home.php">Home</a></li>
+			<li class="li"><a href="about.php">About</a></li>
+			<li class="li"><a href="contact.php">Contact</a></li>
+		</ul>
+	</nav>	
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kawaii</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width=device-width"/>
+</head>
+<body>
+	<nav>
+		<ul class="ul">
+			<li class="li"><a href="about.php">About</a></li>
+			<li class="li"><a href="gallery.php">Gallery</a></li> 
+			<li class="li"><a href="contact.php">Contact</a></li>
+		</ul>
+	</nav>	
+
+	<div id="div">
+		<div id="text">
+			Landing page
+		</div>
+		<div id="cursive">
+			by elizabeth espeleta
+		</div>
+	</div>
+	
+		<div id="bar">
+		</div>
+
+	<div class="bar">
+	</div>
+
+
+</body>
+</html>
